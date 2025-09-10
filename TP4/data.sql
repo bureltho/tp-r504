7c98648118d5779c17aa4474c2a9e49981bc4026
bureltho <bureltho.thomas.burel06@gmail.com>
2025-09-10 15:09:36 +0200

    tp4

---
+++ TP4/data.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE demosql;
+USE demosql;
+CREATE TABLE myTable(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO myTable (id, name) VALUES (NULL, 'bob');
+INSERT INTO myTable (id, name) VALUES (NULL, 'alice');
+INSERT INTO myTable (id, name) VALUES (NULL, 'john');
+
